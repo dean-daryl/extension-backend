e546268783158dc8ff5f54211b79a78d88f3d55f
Dean Daryl MURENZI <deandaryl7@gmail.com>
2024-11-18 11:19:31 +0200

    ch: setup

---
+++ src/main/java/com/example/somatekbackend/SomaTekBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.somatekbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomaTekBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SomaTekBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/somatekbackend/SomaTekBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.somatekbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomaTekBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
